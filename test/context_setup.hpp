13887e8ed5e8a1360731615dad2253d2131ff49a
Denis Demidov <ddemidov@ksu.ru>
2013-04-19 15:15:57 +0400

    Rely on system::default_context() to hold static context
    refs kylelutz/compute#9

--- test/context_setup.hpp
+++ test/context_setup.hpp
@@ -4,36 +4,17 @@
 #include <boost/compute/command_queue.hpp>
 
 struct Context {
-    static boost::compute::device        device;
-    static boost::compute::context       context;
-    static boost::compute::command_queue queue;
+    boost::compute::device        device;
+    boost::compute::context       context;
+    boost::compute::command_queue queue;
 
-    Context() {
-        device  = boost::compute::system::default_device();
-        context = boost::compute::system::default_context();
-        queue   = boost::compute::command_queue(context, device);
-
-        std::cout << device.name() << std::endl;
-    }
-};
-
-boost::compute::device        Context::device;
-boost::compute::context       Context::context;
-boost::compute::command_queue Context::queue;
-
-struct ContextRef {
-    boost::compute::device        &device;
-    boost::compute::context       &context;
-    boost::compute::command_queue &queue;
-
-    ContextRef() :
-        device ( Context::device  ),
-        context( Context::context ),
-        queue  ( Context::queue   )
+    Context() :
+        device ( boost::compute::system::default_device() ),
+        context( boost::compute::system::default_context() ),
+        queue  ( context, device )
     {}
 };
 
-BOOST_GLOBAL_FIXTURE( Context )
-BOOST_FIXTURE_TEST_SUITE(compute_test, ContextRef)
+BOOST_FIXTURE_TEST_SUITE(compute_test, Context)
 
 #endif
