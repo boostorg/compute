6499e30c66f1c0d6f77eab9250b43512e85bce1d
Kyle Lutz <kyle.r.lutz@gmail.com>
2014-04-11 20:06:52 -0700

    Add test creating a std::future for an event object

--- test/test_event.cpp
+++ test/test_event.cpp
@@ -11,6 +11,12 @@
 #define BOOST_TEST_MODULE TestEvent
 #include <boost/test/unit_test.hpp>
 
+#include <boost/config.hpp>
+
+#if !defined(BOOST_NO_CXX11_HDR_FUTURE) && !defined(BOOST_NO_0X_HDR_FUTURE)
+#include <future>
+#endif // BOOST_NO_CXX11_HDR_FUTURE
+
 #include <boost/compute/event.hpp>
 
 #include "context_setup.hpp"
@@ -38,6 +44,29 @@ BOOST_AUTO_TEST_CASE(event_callback)
     queue.finish();
     BOOST_CHECK_EQUAL(callback_invoked, true);
 }
+
+#if !defined(BOOST_NO_CXX11_HDR_FUTURE) && !defined(BOOST_NO_0X_HDR_FUTURE)
+void BOOST_COMPUTE_CL_CALLBACK
+event_promise_fulfiller_callback(cl_event event, cl_int status, void *user_data)
+{
+    auto *promise = static_cast<std::promise<void> *>(user_data);
+    promise->set_value();
+    delete promise;
+}
+
+BOOST_AUTO_TEST_CASE(event_to_std_future)
+{
+    std::vector<float> vector(1000, 3.14f);
+    boost::compute::buffer buffer(context, 1000 * sizeof(float));
+    auto event = queue.enqueue_write_buffer_async(
+        buffer, 0, 1000 * sizeof(float), vector.data()
+    );
+    auto *promise = new std::promise<void>;
+    std::future<void> future = promise->get_future();
+    event.set_callback(event_promise_fulfiller_callback, CL_COMPLETE, promise);
+    future.wait();
+}
+#endif // BOOST_NO_CXX11_HDR_FUTURE
 #endif // CL_VERSION_1_1
 
 BOOST_AUTO_TEST_SUITE_END()
