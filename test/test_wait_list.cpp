4a507cb3681f8e87f07ec6518ac2be3d46c6ebd0
Kyle Lutz <kyle.r.lutz@gmail.com>
2014-05-22 22:40:04 -0700

    Remove test for variadic wait_list::insert()

--- test/test_wait_list.cpp
+++ test/test_wait_list.cpp
@@ -34,43 +34,6 @@ BOOST_AUTO_TEST_CASE(create_wait_list)
     BOOST_CHECK(events.get_event_ptr() == 0);
 }
 
-#ifndef BOOST_COMPUTE_DETAIL_NO_VARIADIC_TEMPLATES
-BOOST_AUTO_TEST_CASE(variadic_insert)
-{
-    // wait list
-    compute::wait_list events;
-
-    // create host data array
-    int data[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-    // create vector on the device
-    compute::vector<int> vector(8, context);
-
-    // copy each pair of values independently and asynchronously
-    compute::event copy1 = queue.enqueue_write_buffer_async(
-        vector.get_buffer(), 0 * sizeof(int), 2 * sizeof(int), data + 0
-    );
-    compute::event copy2 = queue.enqueue_write_buffer_async(
-        vector.get_buffer(), 2 * sizeof(int), 2 * sizeof(int), data + 2
-    );
-    compute::event copy3 = queue.enqueue_write_buffer_async(
-        vector.get_buffer(), 4 * sizeof(int), 2 * sizeof(int), data + 4
-    );
-    compute::event copy4 = queue.enqueue_write_buffer_async(
-        vector.get_buffer(), 6 * sizeof(int), 2 * sizeof(int), data + 6
-    );
-
-    // add all events to the wait list
-    events.insert(copy1, copy2, copy3, copy4);
-
-    // block until all events complete
-    events.wait();
-
-    // check
-    CHECK_RANGE_EQUAL(int, 8, vector, (1, 2, 3, 4, 5, 6, 7, 8));
-}
-#endif // BOOST_COMPUTE_DETAIL_NO_VARIADIC_TEMPLATES
-
 BOOST_AUTO_TEST_CASE(insert_future)
 {
     // create vector on the host
