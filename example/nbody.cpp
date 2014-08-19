0dcbc9721525a8eb7cda71a528929c567450ef4c
f-koehler <fabian2804@googlemail.com>
2014-08-19 23:08:12 +0200

    nbody example has command line arguments by now

--- example/nbody.cpp
+++ example/nbody.cpp
@@ -23,6 +23,7 @@
 #include <QtOpenGL>
 #include <QTimer>
 
+#include <boost/program_options.hpp>
 #include <boost/random/uniform_real_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 
@@ -32,9 +33,9 @@
 #include <boost/compute/source.hpp>
 
 namespace compute = boost::compute;
+namespace po = boost::program_options;
 
-const uint N   = 50000;
-const float dt = 0.0001f;
+using compute::uint_;
 
 const char source[] = BOOST_COMPUTE_STRINGIZE_SOURCE(
     __kernel void initVelocity(__global float* velocity)
@@ -85,7 +86,7 @@ class NBodyWidget : public QGLWidget
     Q_OBJECT
 
 public:
-    NBodyWidget(QWidget* parent = 0);
+    NBodyWidget(std::size_t particles, float dt, QWidget* parent = 0);
     ~NBodyWidget();
 
     void initializeGL();
@@ -106,10 +107,13 @@ private:
     compute::kernel m_position_kernel;
 
     bool m_initial_draw;
+
+    const uint_ m_particles;
+    const float m_dt;
 };
 
-NBodyWidget::NBodyWidget(QWidget* parent)
-    : QGLWidget(parent), m_initial_draw(true)
+NBodyWidget::NBodyWidget(std::size_t particles, float dt, QWidget* parent)
+    : m_initial_draw(true), m_particles(particles), m_dt(dt), QGLWidget(parent)
 {
     // create a timer to redraw as fast as possible
     timer = new QTimer(this);
@@ -133,10 +137,10 @@ void NBodyWidget::initializeGL()
     m_program.build();
 
     // prepare random particle positions that will be transferred to the vbo
-    float* temp = new float[N*3];
+    float* temp = new float[m_particles*3];
     boost::random::uniform_real_distribution<float> dist(-0.5f, 0.5f);
     boost::random::mt19937_64 gen;
-    for(size_t i = 0; i < N*3; i++) {
+    for(size_t i = 0; i < m_particles*3; i++) {
         temp[i] = dist(gen);
     }
 
@@ -144,31 +148,31 @@ void NBodyWidget::initializeGL()
     GLuint vbo = 0;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 3*N*sizeof(float), temp, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3*m_particles*sizeof(float), temp, GL_DYNAMIC_DRAW);
 
     // create a OpenCL buffer from the vbo
     m_position = compute::opengl_buffer(m_context, vbo);
     delete[] temp;
 
     // create buffer for velocities
-    m_velocity = compute::buffer(m_context, 3*N*sizeof(float));
+    m_velocity = compute::buffer(m_context, 3*m_particles*sizeof(float));
 
     // make sure velocities are 0
     compute::kernel init_kernel = m_program.create_kernel("initVelocity");
     init_kernel.set_arg(0, m_velocity);
-    m_queue.enqueue_1d_range_kernel(init_kernel, 0, N, 0);
+    m_queue.enqueue_1d_range_kernel(init_kernel, 0, m_particles, 0);
     m_queue.finish();
 
     // create compute kernels
     m_velocity_kernel = m_program.create_kernel("updateVelocity");
     m_velocity_kernel.set_arg(0, m_position);
     m_velocity_kernel.set_arg(1, m_velocity);
-    m_velocity_kernel.set_arg(2, dt);
-    m_velocity_kernel.set_arg(3, N);
+    m_velocity_kernel.set_arg(2, m_dt);
+    m_velocity_kernel.set_arg(3, m_particles);
     m_position_kernel = m_program.create_kernel("updatePosition");
     m_position_kernel.set_arg(0, m_position);
     m_position_kernel.set_arg(1, m_velocity);
-    m_position_kernel.set_arg(2, dt);
+    m_position_kernel.set_arg(2, m_dt);
 }
 void NBodyWidget::resizeGL(int width, int height)
 {
@@ -179,9 +183,6 @@ void NBodyWidget::paintGL()
 {
     // clear buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    float w = width();
-    float h = height();
 
     // check if this is the first draw
     if(m_initial_draw) {
@@ -195,15 +196,15 @@ void NBodyWidget::paintGL()
     // draw
     glVertexPointer(3, GL_FLOAT, 0, 0);
     glEnableClientState(GL_VERTEX_ARRAY);
-    glDrawArrays(GL_POINTS, 0, N);
+    glDrawArrays(GL_POINTS, 0, m_particles);
     glFinish();
 }
 void NBodyWidget::updateParticles()
 {
     // enqueue kernels to update particles and make sure that the command queue is finished
     compute::opengl_enqueue_acquire_buffer(m_position, m_queue);
-    m_queue.enqueue_1d_range_kernel(m_velocity_kernel, 0, N, 0).wait();
-    m_queue.enqueue_1d_range_kernel(m_position_kernel, 0, N, 0).wait();
+    m_queue.enqueue_1d_range_kernel(m_velocity_kernel, 0, m_particles, 0).wait();
+    m_queue.enqueue_1d_range_kernel(m_position_kernel, 0, m_particles, 0).wait();
     m_queue.finish();
     compute::opengl_enqueue_release_buffer(m_position, m_queue);
 }
@@ -216,8 +217,26 @@ void NBodyWidget::keyPressEvent(QKeyEvent* event)
 
 int main(int argc, char** argv)
 {
+    // parse command line arguments
+    po::options_description options("options");
+    options.add_options()
+        ("help", "show usage")
+        ("particles", po::value<uint_>()->default_value(1000), "number of particles")
+        ("dt", po::value<float>()->default_value(0.001f), "width of each integration step");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, options), vm);
+    po::notify(vm);
+
+    if(vm.count("help") > 0) {
+        std::cout << options << std::endl;
+        return 0;
+    }
+
+    const uint_ particles = vm["particles"].as<uint_>();
+    const float dt = vm["dt"].as<float>();
+
     QApplication app(argc, argv);
-    NBodyWidget nbody;
+    NBodyWidget nbody(particles, dt);
 
     nbody.show();
 
