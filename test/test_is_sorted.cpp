744359715f5a7e6970fc39b28c434a43e2d59af7
Kyle Lutz <kyle.r.lutz@gmail.com>
2014-08-21 22:35:33 -0700

    Implement is_sorted() with adjacent_find()

--- test/test_is_sorted.cpp
+++ test/test_is_sorted.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2013 Kyle Lutz <kyle.r.lutz@gmail.com>
+// Copyright (c) 2013-2014 Kyle Lutz <kyle.r.lutz@gmail.com>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
@@ -12,40 +12,60 @@
 #include <boost/test/unit_test.hpp>
 
 #include <boost/compute/system.hpp>
-#include <boost/compute/algorithm/sort.hpp>
+#include <boost/compute/algorithm/iota.hpp>
 #include <boost/compute/algorithm/is_sorted.hpp>
+#include <boost/compute/algorithm/sort.hpp>
+#include <boost/compute/algorithm/reverse.hpp>
 #include <boost/compute/container/vector.hpp>
 
 #include "context_setup.hpp"
 
-namespace bc = boost::compute;
+namespace compute = boost::compute;
 
 BOOST_AUTO_TEST_CASE(is_sorted_int)
 {
-    bc::vector<int> vector;
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == true);
+    compute::vector<int> vec(context);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
 
-    vector.push_back(1);
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == true);
+    vec.push_back(1, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
 
-    vector.push_back(2);
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == true);
+    vec.push_back(2, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
 
-    vector.push_back(0);
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == false);
+    vec.push_back(0, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue) == false);
 
-    vector.push_back(-2);
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == false);
+    vec.push_back(-2, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue) == false);
 
-    bc::sort(vector.begin(), vector.end());
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == true);
+    compute::sort(vec.begin(), vec.end(), queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
 }
 
 BOOST_AUTO_TEST_CASE(is_sorted_ones)
 {
-    bc::vector<int> vector(10);
-    bc::fill(vector.begin(), vector.end(), int(1));
-    BOOST_VERIFY(bc::is_sorted(vector.begin(), vector.end()) == true);
+    compute::vector<int> vec(2048, context);
+    compute::fill(vec.begin(), vec.end(), 1, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
+}
+
+BOOST_AUTO_TEST_CASE(is_sorted_iota)
+{
+    // create vector with values from 1..1000
+    compute::vector<int> vec(1000, context);
+    compute::iota(vec.begin(), vec.end(), 1, queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
+
+    // reverse the range
+    compute::reverse(vec.begin(), vec.end(), queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue) == false);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), compute::greater<int>(), queue));
+
+    // reverse it back
+    compute::reverse(vec.begin(), vec.end(), queue);
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), queue));
+    BOOST_CHECK(compute::is_sorted(vec.begin(), vec.end(), compute::greater<int>(), queue) == false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
