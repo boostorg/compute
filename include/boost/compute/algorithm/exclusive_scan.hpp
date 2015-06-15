7bea0b13a1f6ceb576c1750626e15b8b520ca5b6
Jakub Szuppe <j.szuppe@gmail.com>
2015-06-15 20:59:27 +0200

    Support for a custom function in exclusive and inclusive scan

--- include/boost/compute/algorithm/exclusive_scan.hpp
+++ include/boost/compute/algorithm/exclusive_scan.hpp
@@ -11,6 +11,7 @@
 #ifndef BOOST_COMPUTE_ALGORITHM_EXCLUSIVE_SCAN_HPP
 #define BOOST_COMPUTE_ALGORITHM_EXCLUSIVE_SCAN_HPP
 
+#include <boost/compute/functional.hpp>
 #include <boost/compute/system.hpp>
 #include <boost/compute/command_queue.hpp>
 #include <boost/compute/algorithm/detail/scan.hpp>
@@ -27,13 +28,53 @@ namespace compute {
 /// \param first first element in the range to scan
 /// \param last last element in the range to scan
 /// \param result first element in the result range
+/// \param init value used to initialize the scan sequence
+/// \param binary_op associative binary operator
 /// \param queue command queue to perform the operation
 ///
 /// \return \c OutputIterator to the end of the result range
 ///
+/// The default operation is to add the elements up.
+///
 /// \snippet test/test_scan.cpp exclusive_scan_int
 ///
+/// But different associative operation can be specified as \p binary_op
+/// instead (e.g., multiplication, maximum, minimum). Also value used to
+/// initialized the scan sequence can be specified.
+///
+/// \snippet test/test_scan.cpp exclusive_scan_int_multiplies
+///
 /// \see inclusive_scan()
+template<class InputIterator, class OutputIterator, class T, class BinaryOperator>
+inline OutputIterator
+exclusive_scan(InputIterator first,
+               InputIterator last,
+               OutputIterator result,
+               T init,
+               BinaryOperator binary_op,
+               command_queue &queue = system::default_queue())
+{
+    return detail::scan(first, last, result, true, init, binary_op, queue);
+}
+
+/// \overload
+template<class InputIterator, class OutputIterator, class T>
+inline OutputIterator
+exclusive_scan(InputIterator first,
+               InputIterator last,
+               OutputIterator result,
+               T init,
+               command_queue &queue = system::default_queue())
+{
+    typedef typename
+        std::iterator_traits<OutputIterator>::value_type output_type;
+
+    return detail::scan(first, last, result, true,
+                        init, boost::compute::plus<output_type>(),
+                        queue);
+}
+
+/// \overload
 template<class InputIterator, class OutputIterator>
 inline OutputIterator
 exclusive_scan(InputIterator first,
@@ -41,7 +82,12 @@ exclusive_scan(InputIterator first,
                OutputIterator result,
                command_queue &queue = system::default_queue())
 {
-    return detail::scan(first, last, result, true, queue);
+    typedef typename
+        std::iterator_traits<OutputIterator>::value_type output_type;
+
+    return detail::scan(first, last, result, true,
+                        output_type(0), boost::compute::plus<output_type>(),
+                        queue);
 }
 
 } // end compute namespace
