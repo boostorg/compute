7bc46aba98a256350017a737d0b4453b59089523
Jakub Szuppe <j.szuppe@gmail.com>
2015-07-11 22:12:36 +0200

    Parallel reduce by key algorithm implementation

--- include/boost/compute/algorithm/detail/reduce_by_key.hpp
+++ include/boost/compute/algorithm/detail/reduce_by_key.hpp
@@ -19,12 +19,47 @@
 #include <boost/compute/container/vector.hpp>
 #include <boost/compute/detail/iterator_range_size.hpp>
 #include <boost/compute/algorithm/detail/serial_reduce_by_key.hpp>
+#include <boost/compute/algorithm/detail/reduce_by_key_with_scan.hpp>
 #include <boost/compute/type_traits.hpp>
-#include <boost/compute/utility/program_cache.hpp>
 
 namespace boost {
 namespace compute {
 namespace detail {
+
+template<class InputKeyIterator, class InputValueIterator,
+         class OutputKeyIterator, class OutputValueIterator,
+         class BinaryFunction, class BinaryPredicate>
+size_t reduce_by_key_on_gpu(InputKeyIterator keys_first,
+                            InputKeyIterator keys_last,
+                            InputValueIterator values_first,
+                            OutputKeyIterator keys_result,
+                            OutputValueIterator values_result,
+                            BinaryFunction function,
+                            BinaryPredicate predicate,
+                            command_queue &queue)
+{
+    return detail::reduce_by_key_with_scan(keys_first, keys_last, values_first,
+                                           keys_result, values_result, function,
+                                           predicate, queue);
+}
+
+template<class InputKeyIterator, class InputValueIterator,
+         class OutputKeyIterator, class OutputValueIterator>
+bool reduce_by_key_on_gpu_requirements_met(InputKeyIterator keys_first,
+                                           InputValueIterator values_first,
+                                           OutputKeyIterator keys_result,
+                                           OutputValueIterator values_result,
+                                           const size_t count,
+                                           command_queue &queue)
+{
+    const device &device = queue.get_device();
+    return (count > 256)
+               && !(device.type() & device::cpu)
+               && reduce_by_key_with_scan_requirements_met(keys_first, values_first,
+                                                           keys_result,values_result,
+                                                           count, queue);
+    return true;
+}
 
 template<class InputKeyIterator, class InputValueIterator,
          class OutputKeyIterator, class OutputValueIterator,
@@ -55,11 +90,20 @@ dispatch_reduce_by_key(InputKeyIterator keys_first,
             );
     }
 
-    size_t result_size =
-        detail::serial_reduce_by_key(keys_first, keys_last, values_first,
-                                     keys_result, values_result, function,
-                                     predicate, queue);
-
+    size_t result_size = 0;
+    if(reduce_by_key_on_gpu_requirements_met(keys_first, values_first, keys_result,
+                                             values_result, count, queue)){
+        result_size =
+            detail::reduce_by_key_on_gpu(keys_first, keys_last, values_first,
+                                         keys_result, values_result, function,
+                                         predicate, queue);
+    }
+    else {
+        result_size =
+              detail::serial_reduce_by_key(keys_first, keys_last, values_first,
+                                           keys_result, values_result, function,
+                                           predicate, queue);
+    }
 
     return
         std::make_pair<OutputKeyIterator, OutputValueIterator>(
