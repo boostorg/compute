4b65b28a7fa44e6d083586f4dea8f66db9dea4b8
Jakub Szuppe <j.szuppe@gmail.com>
2015-07-16 20:58:47 +0200

    Add missing Boost license info and copyrights

--- perf/perf_thrust_exclusive_scan.cu
+++ perf/perf_thrust_exclusive_scan.cu
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2013-2014 Kyle Lutz <kyle.r.lutz@gmail.com>
+// Copyright (c) 2014 Benoit
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
