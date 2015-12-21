301c541824daf37978dabe66344767cfaa36455b
Kyle Lutz <kyle.r.lutz@gmail.com>
2015-12-20 18:34:14 -0800

    Release v0.5

--- include/boost/compute/version.hpp
+++ include/boost/compute/version.hpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2013 Kyle Lutz <kyle.r.lutz@gmail.com>
+// Copyright (c) 2013-2015 Kyle Lutz <kyle.r.lutz@gmail.com>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
@@ -12,7 +12,7 @@
 #define BOOST_COMPUTE_VERSION_HPP
 
 #define BOOST_COMPUTE_VERSION_MAJOR 0
-#define BOOST_COMPUTE_VERSION_MINOR 4
+#define BOOST_COMPUTE_VERSION_MINOR 5
 #define BOOST_COMPUTE_VERSION_PATCH 0
 
 #endif // BOOST_COMPUTE_VERSION_HPP
