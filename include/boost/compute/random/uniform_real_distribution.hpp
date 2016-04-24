8aea29032044072080668898ac46bc4393e13cd7
Jakub Szuppe <j.szuppe@gmail.com>
2016-04-24 21:55:14 +0200

    Make sure a < b in uniform real distribution
    Making sure that the left endpoint (a) of the range in uniform
    real distribution is less than the right endpoint (b).

--- include/boost/compute/random/uniform_real_distribution.hpp
+++ include/boost/compute/random/uniform_real_distribution.hpp
@@ -11,6 +11,8 @@
 #ifndef BOOST_COMPUTE_RANDOM_UNIFORM_REAL_DISTRIBUTION_HPP
 #define BOOST_COMPUTE_RANDOM_UNIFORM_REAL_DISTRIBUTION_HPP
 
+#include <boost/assert.hpp>
+
 #include <boost/compute/command_queue.hpp>
 #include <boost/compute/function.hpp>
 #include <boost/compute/detail/literal.hpp>
@@ -36,10 +38,12 @@ public:
 
     /// Creates a new uniform distribution producing numbers in the range
     /// [\p a, \p b).
+    /// Requires a < b
     uniform_real_distribution(RealType a = 0.f, RealType b = 1.f)
         : m_a(a),
           m_b(b)
     {
+        BOOST_ASSERT(a < b);
     }
 
     /// Destroys the uniform_real_distribution object.
