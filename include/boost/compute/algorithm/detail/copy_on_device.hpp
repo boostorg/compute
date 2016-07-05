c6123c403c45d0d4678f12110192f828cd560918
Jakub Szuppe <j.szuppe@gmail.com>
2016-07-05 13:40:42 +0200

    Remove unused function

--- include/boost/compute/algorithm/detail/copy_on_device.hpp
+++ include/boost/compute/algorithm/detail/copy_on_device.hpp
@@ -27,18 +27,6 @@ namespace boost {
 namespace compute {
 namespace detail {
 
-inline size_t pick_copy_work_group_size(size_t n, const device &device)
-{
-    (void) device;
-
-    if(n % 32 == 0) return 32;
-    else if(n % 16 == 0) return 16;
-    else if(n % 8 == 0) return 8;
-    else if(n % 4 == 0) return 4;
-    else if(n % 2 == 0) return 2;
-    else return 1;
-}
-
 template<class InputIterator, class OutputIterator>
 class copy_kernel : public meta_kernel
 {
