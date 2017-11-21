01007332300be409b4f5d78bcdc9bbfb5e5d1853
Kristian Popov <kristian.popov@outlook.com>
2017-11-21 18:21:09 +0100

    Added copyright notice to new files

--- include/boost/compute/exception/program_build_failure.hpp
+++ include/boost/compute/exception/program_build_failure.hpp
@@ -1,3 +1,12 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2017 Kristian Popov <kristian.popov@outlook.com>
+//
+// Distributed under the Boost Software License, Version 1.0
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
+// See http://boostorg.github.com/compute for more information.
+//---------------------------------------------------------------------------//
 #ifndef BOOST_COMPUTE_EXCEPTION_PROGRAM_BUILD_FAILURE_HPP
 #define BOOST_COMPUTE_EXCEPTION_PROGRAM_BUILD_FAILURE_HPP
 
