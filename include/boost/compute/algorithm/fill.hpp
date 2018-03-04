d833b22bb157ca90b5b6cb04fa0e97b893261cbd
Lakshay Garg <lakshayg@outlook.in>
2018-03-04 16:59:49 +0530

    Add is_device_asserts

--- include/boost/compute/algorithm/fill.hpp
+++ include/boost/compute/algorithm/fill.hpp
@@ -28,6 +28,8 @@
 #include <boost/compute/iterator/discard_iterator.hpp>
 #include <boost/compute/detail/is_buffer_iterator.hpp>
 #include <boost/compute/detail/iterator_range_size.hpp>
+#include <boost/compute/type_traits/is_device_iterator.hpp>
+
 
 namespace boost {
 namespace compute {
@@ -281,6 +283,7 @@ inline void fill(BufferIterator first,
                  const T &value,
                  command_queue &queue = system::default_queue())
 {
+    BOOST_STATIC_ASSERT(is_device_iterator<BufferIterator>::value);
     size_t count = detail::iterator_range_size(first, last);
     if(count == 0){
         return;
--- include/boost/compute/algorithm/iota.hpp
+++ include/boost/compute/algorithm/iota.hpp
@@ -11,10 +11,13 @@
 #ifndef BOOST_COMPUTE_ALGORITHM_IOTA_HPP
 #define BOOST_COMPUTE_ALGORITHM_IOTA_HPP
 
+#include <boost/static_assert.hpp>
+
 #include <boost/compute/system.hpp>
 #include <boost/compute/command_queue.hpp>
 #include <boost/compute/algorithm/copy.hpp>
 #include <boost/compute/iterator/counting_iterator.hpp>
+#include <boost/compute/type_traits/is_device_iterator.hpp>
 
 namespace boost {
 namespace compute {
@@ -34,6 +37,7 @@ inline void iota(BufferIterator first,
                  const T &value,
                  command_queue &queue = system::default_queue())
 {
+    BOOST_STATIC_ASSERT(is_device_iterator<BufferIterator>::value);
     T count = static_cast<T>(detail::iterator_range_size(first, last));
 
     copy(
